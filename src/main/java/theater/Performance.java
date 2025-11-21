284d1924d8dcfe26dcbed6f4a83990be075782b6
yunji4 <yunji5v@gmail.com>
2025-11-21 17:09:45 -0500

    Task 1.1

--- src/main/java/theater/Performance.java
+++ src/main/java/theater/Performance.java
@@ -1,15 +1,31 @@
 package theater;
 
 /**
- * Class representing a performance of a play..
+ * Class representing a performance of a play.
  */
 public class Performance {
 
-    public String playID;
-    public int audience;
+    private String playID;
+    private final int audience;
 
     public Performance(String playID, int audience) {
         this.playID = playID;
         this.audience = audience;
+    }
+
+    /**
+     * Gets the play ID.
+     * @return the play ID
+     */
+    public String getPlayID() {
+        return playID;
+    }
+
+    /**
+     * Getter for audiennce.
+     * @return the number of audience
+     */
+    public int getAudience() {
+        return audience;
     }
 }
--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,34 @@
 package theater;
 
+/**
+ * Class built for plays.
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private String name;
+    private String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public Play() {
+    }
+
+    /**
+     * Getter for name.
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Getter for name.
+     * @return the name
+     */
+    public String getType() {
+        return type;
     }
 }
